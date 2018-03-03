5e0cdd45d066084184fe08781021a30b802d148c
Roy Chan <roychanckc@gmail.com>
2018-03-03 21:17:44 +0800

    Test Post /todos route

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
-  
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -25,3 +25,7 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {
+  app
+};
