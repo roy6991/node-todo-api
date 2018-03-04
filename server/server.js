5aaacff812ca383280136b34e5337c6a0d406b6c
Roy Chan <roychanckc@gmail.com>
2018-03-04 22:44:47 +0800

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -51,6 +51,29 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+    Todo.findByIdAndRemove(id).then(todo => {
+      if (!todo) {
+        res.status(404).send()
+      }
+      res.send(todo);
+    });
+  }).catch(e => {
+    res.status(404).send();
+  });
+});
+
 app.listen(port, () => {
   console.log('Started on port ' + port);
 });
